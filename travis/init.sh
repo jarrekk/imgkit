5234f0828eb7c47955b3301283e56cf8b6bda7f1
Jack <me@jack003.com>
2017-01-30 13:30:28 +0800

    edit sh

--- travis/init.sh
+++ travis/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y openssl build-essential xorg libssl-dev
+apt-get install -y openssl build-essential xorg libssl-dev
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 tar -xJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 cd wkhtmltox
