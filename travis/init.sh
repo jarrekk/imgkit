ee9a627bbcf8a3e92de633813577b5e7f39ccedd
Jack <me@jack003.com>
2017-01-30 13:27:21 +0800

    edit sh

--- travis/init.sh
+++ travis/init.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y openssl build-essential xorg libssl-dev
 wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-sudo tar -xJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+tar -xJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 cd wkhtmltox
 sudo chown root:root bin/wkhtmltopdf
 sudo cp -r * /usr/
