152aa033fed08e26a3e5abbb6d50afdd67e68d3c
Jack <me@jarrekk.com>
2017-06-08 15:54:15 +0800

    edit test

--- imgkit/imgkit.py
+++ imgkit/imgkit.py
@@ -251,12 +251,8 @@ class IMGKit(object):
             return stdout
         else:
             try:
-                with codecs.open(path) as f:
-                    # if sys.version.startswith('3.4') or sys.version.startswith('3.5'):
-                    #     text = f.read(4).encode('utf16')
-                    # else:
-                    #     text = f.read(4)
-                    text = f.read()
+                with codecs.open(path, mode='rb') as f:
+                    text = f.read(4)
                     if text == '':
                         raise IOError('Command failed: %s\n'
                                       'Check whhtmltoimage output without \'quiet\' '
