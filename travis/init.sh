2abea0d443d177e673c1f5769999e5d98ca80074
Jack <me@jarrekk.com>
2017-06-29 10:38:00 +0800

    add coverage

--- travis/init.sh
+++ travis/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y openssl build-essential xorg libssl-dev xvfb
+pip install coverage
 wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 cd wkhtmltox
