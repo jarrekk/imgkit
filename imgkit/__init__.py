a09dc9886f0e08405b358a2d123c21e45d426218
Jarrekk <me@jarrekk.com>
2021-03-13 22:50:57 +0800

    update version

--- imgkit/__init__.py
+++ imgkit/__init__.py
@@ -5,7 +5,7 @@ Wkhtmltopdf python wrapper to convert html to image using the webkit rendering e
 
 __author__ = 'jarrekk'
 __contact__ = 'me@jarrekk.com'
-__version__ = '1.0.2'
+__version__ = '1.0.5'
 __homepage__ = 'https://github.com/jarrekk/imgkit'
 __license__ = 'MIT'
 
